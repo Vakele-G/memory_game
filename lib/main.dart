89a480b2b22cdb659cf3204fe2d86fc52808b971
Vakele Godongwana <vakelegawd@gmail.com>
2026-09-02 15:26:40 +0200

    feat: create Tile() class and replace the cloud icons with tiles. Add scaffold as home of the MaterialApp, and make the body expanded (take full space)

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          for (int j = 1; j <= 8; j++)
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [for (int i = 1; i <= 8; i++) Icon(Icons.cloud)],
-            ),
-        ],
-      ),
+      home: Scaffold(
+        body: Expanded(child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 5,
+          children: [
+            for (int j = 1; j <= 7; j++)
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 5,
+                children: [for (int i = 1; i <= 7; i++) Tile()],
+              ),
+          ],
+        ),)
+
+      )
     );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      SizedBox(
+        width: 50,
+        height: 50,
+        child: Card(
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text("A")],
+          ),
+        )
+      );
   }
 }
