b4a2efc731be46c18953372349bd334e90078be9
Vakele Godongwana <vakelegawd@gmail.com>
2026-09-23 21:55:21 +0200

    Add TileWidget class to that handles the visual representation and flip animation

---
+++ lib/tile_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'tile_model.dart';
+
+class TileWidget extends StatelessWidget {
+  final TileModel tile;
+
+  const TileWidget({super.key, required this.tile});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 300),
+      transitionBuilder: (Widget child, Animation<double> animation) {
+        return ScaleTransition(scale: animation, child: child);
+      },
+
+      child: tile.isFlipped
+          ? Card(
+              key: const ValueKey("front"),
+              color: Colors.blueAccent,
+              child: Center(
+                child: Text(
+                  tile.letter ?? "",
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
+          : const Card(key: ValueKey("back"), color: Colors.grey),
+    );
+  }
+}
